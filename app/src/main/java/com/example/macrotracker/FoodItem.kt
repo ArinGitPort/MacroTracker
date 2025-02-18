3a3793c56aec00870c26fb889954cf769f87532e
Allen <rimuesencho@gmail.com>
2025-02-19 03:28:05 +0800

    1st Commit

--- app/src/main/java/com/example/macrotracker/FoodItem.kt
+++ app/src/main/java/com/example/macrotracker/FoodItem.kt
@@ -5,5 +5,7 @@ data class FoodItem(
     val calories: Int = 0,
     val protein: Int = 0,
     val carbs: Int = 0,
-    val fats: Int = 0
+    val fats: Int = 0,
+    var servingSize: Double = 1.0,  // Default is 1 serving
+    var unit: String = "serving"  // Can be "g" or "serving"
 )
